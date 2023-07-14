38e1566c5312e94f9b30f859465c7c2e51f45be0
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-07-14 16:00:30 -0300

    Feature: function process completed

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -15,23 +15,6 @@ def txt_importer(path_file):
         sys.stderr.write("Formato inválido")
         return []
 
-    # length_path_file = len(path_file)
-
-    # correction_to_last_index = 1
-
-    # last_index = length_path_file - correction_to_last_index
-    # start_index = last_index - length_format
-
-    # path_format = ''
-
-    # length_format = 4
-
-    # Verificar se formato do arquivo é .txt
-    # for i in range(start_index, last_index, 1 ):
-    #     path_format[i] = path_file[i]
-    # if path_format != '.txt':
-    #     raise IndexError('Formato inválido na stderr')
-
     # retorna uma lista contendo as linhas do arquivo
 
     with open(path_file, 'r') as file:
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,29 @@
+from .file_management import txt_importer
+# import sys
+# sys.path.append('ting_file_management/file_management.py/diretorio')
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # path_file_is_in_instamce = False
+    for item in instance._data:
+        if item['nome_do_arquivo'] == path_file:
+            # path_file_is_in_instamce = True
+            print(f"Arquivo {path_file} já existente na fila. Ignorando...")
+            return None
+
+    # if path_file_is_in_instamce == False:
+    lines = txt_importer(path_file)
+    new_dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines,
+    }
+
+    instance.enqueue(new_dict)
+
+    print(new_dict)
+
+    # return new_dict
 
 
 def remove(instance):

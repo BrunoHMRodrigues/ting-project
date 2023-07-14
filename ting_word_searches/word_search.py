d95ba8cce0e8e97384b659b745f684ab68af6a4b
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-07-14 16:39:45 -0300

    Feature: Function exists_word completed

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance._data:
+        ocorrencias = []
+        # for index in range(0, len(item['linhas_do_arquivo']), 1)
+        #     if line.find(word) != -1:
+        for index, line in enumerate(item['linhas_do_arquivo'], 1):
+            if line.lower().find(word.lower()) != -1:
+                ocorrencias.append({'linha': index})
+
+        if len(ocorrencias) > 0:
+            result.append({
+                'palavra': word,
+                'arquivo': item['nome_do_arquivo'],
+                'ocorrencias': ocorrencias,
+            })
+    return result
 
 
 def search_by_word(word, instance):
